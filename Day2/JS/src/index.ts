23732d5023180c848f709f36bb5ce32bbb2aee19
Varun Singh <varunsingh7004@gmail.com>
2025-08-24 10:21:31 +0530

    added users and todos routes

--- Day2/JS/src/index.ts
+++ Day2/JS/src/index.ts
@@ -1,3 +1,21 @@
 import express from "express";
 const app = express();
 app.use(express.json());
+
+// User routes
+app.post("/users", async (req, res) => {});
+
+app.get("/users/:id", async (req, res) => {});
+
+app.put("/users/:id", async (req, res) => {});
+
+app.delete("/users/:id", async (req, res) => {});
+
+// Todos
+app.post("/todos", async (req, res) => {});
+
+app.get("/todos/:id", async (req, res) => {});
+
+app.put("/todos/:id", async (req, res) => {});
+
+app.delete("/todos/:id", async (req, res) => {});
