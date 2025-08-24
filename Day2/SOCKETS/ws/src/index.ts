e062ba3ea8b849f9a85996fb6597ca08954cfa03
Varun Singh <varunsingh7004@gmail.com>
2025-08-24 12:15:24 +0530

    ws start

--- Day2/SOCKETS/ws/src/index.ts
+++ Day2/SOCKETS/ws/src/index.ts
@@ -1,2 +1,13 @@
-import WebSocket from "ws";
+import { WebSocketServer } from "ws";
 
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function (ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function (data) {
+    console.log("received: %s", data);
+  });
+
+  ws.send("something");
+});
